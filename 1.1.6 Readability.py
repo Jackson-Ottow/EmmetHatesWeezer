e55bc9cfa790f76147e2dd997eb231218ab65faf
Jackson-Ottow <113128378+Jackson-Ottow@users.noreply.github.com>
2022-09-29 09:46:21 -0700

    Add files via upload

---
+++ 1.1.6 Readability.py
@@ -0,0 +1,23 @@
+#   a116_buggy_image.py
+import turtle as trtl
+# instead of a descriptive name of the turtle such as painter,
+# a less useful variable name x is used
+spider = trtl.Turtle()
+#create a spider body
+spider.pensize(40)
+spider.circle(20)
+#configure legs
+legs = 8
+leg_length = 120
+leg_angle = 200 / legs
+spider.pensize(5)
+remaining = 0
+#draw legs
+while (remaining < legs):
+  spider.goto(0,0)
+  spider.setheading(leg_angle*remaining)
+  spider.forward(leg_length)
+  remaining = remaining + 1
+spider.hideturtle()
+wn = trtl.Screen()
+wn.mainloop()
